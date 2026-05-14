749e7236b246ba477918685d706a5be8eeedbee8
EnzoTatou <enzoberti@protonmail.com>
2026-05-14 15:15:59 +0200

    Add title_screen with functionning buttons

---
+++ sans-transition/gui/title_screen.gd
@@ -0,0 +1,33 @@
+extends Control
+
+@export var tween_intensity: float
+@export var tween_duration: float
+
+@export var sfx_button_pressed: AudioStreamPlayer2D
+@export var start_button: BaseButton
+@export var credits_button: BaseButton
+
+func _process(_delta: float) -> void:
+	btn_hovered(start_button)
+	btn_hovered(credits_button)
+
+func start_tween(object: Object, property: String, final_val: Variant, duration: float):
+	var tween = create_tween()
+	tween.tween_property(object, property, final_val, duration)
+
+func btn_hovered(button: BaseButton):
+	button.pivot_offset = button.size / 2.0
+	if button.is_hovered():
+		start_tween(button, "scale", Vector2.ONE * tween_intensity, tween_duration)
+	else:
+		start_tween(button, "scale", Vector2.ONE, tween_duration)
+
+
+func _on_start_button_pressed() -> void:
+	sfx_button_pressed.play()
+	
+	await get_tree().create_timer(0.2).timeout
+	get_tree().change_scene_to_packed(load("uid://c8mtky32erftq"))
+
+func _on_credits_button_pressed() -> void:
+	pass # Replace with function body.
