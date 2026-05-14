c63088be14e3c8cbdf9939b2647b94446f64c576
EnzoTatou <enzoberti@protonmail.com>
2026-05-14 21:56:34 +0200

    add animation to crowd

--- sans-transition/crowd/crowd_excitement.gd
+++ sans-transition/crowd/crowd_excitement.gd
@@ -2,9 +2,25 @@ extends Node
 
 @export var track: TrackData
 
+@export var animations: Array[AnimatedSprite2D]
+@export var animationPlayer: AnimationPlayer
+
 func _ready() -> void:
-	pass
+	for animation in animations:
+		animation.play()
+		var x: float = randf() * 1700 + 100
+		animation.position.x = x
+	
+	animationPlayer.play("crowd")
+	
+	_on_bpm_changed(2)
 
 
 func _process(delta: float) -> void:
 	pass
+
+func _on_bpm_changed(bpm: float) -> void:
+	for animation in animations:
+		animation.speed_scale = bpm
+	
+	animationPlayer.speed_scale = bpm
