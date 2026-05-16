b7125e1d52cd0fbe7e7741caa940ef515f055bd5
Cédric Thiriet-Remondin <thiriet.remondin.cedric@gmail.com>
2026-05-16 09:52:21 +0200

    feat : gestion du scoring with curve

--- sans-transition/scoring.gd
+++ sans-transition/scoring.gd
@@ -1,34 +1,43 @@
-extends Node2D
+extends Node
 
-@export var mix_table: MixtTable
 @export var scoring_label : Label
 @export var ambiance_progress_bar: TextureProgressBar
+
+@export var scoring_curve: Curve
+@export var max_range: float = 7
+var _current_time: float
 
 var current_score: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	current_score = 0.0
-
-	mix_table.scoring_event.connect(
-		func(value:float)->void: 
-			compute_score(value)
-	)
 	
-	mix_table.track_switched.connect(reset_ambiance_bar)
+	reset_ambiance_bar()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	scoring_label.text = str(current_score).pad_decimals(2)
+	var t: float = 1-(_current_time/max_range)
+	print(t)
 	
-	ambiance_progress_bar.value -= delta
-	print("ambiance_progress_bar.value")
-	print(ambiance_progress_bar.value)
-	pass
+	current_score += scoring_curve.sample(t) * delta
+	
+	_current_time -= delta
+	_current_time = max(_current_time,0)
+	
+	update_ui()
 
 func reset_ambiance_bar() -> void:
-	ambiance_progress_bar.value = ambiance_progress_bar.max_value
+	_current_time = max_range
+	
+func update_ui() -> void:
+	var t: float = _current_time/max_range
+	ambiance_progress_bar.value = t
+	scoring_label.text = str(floori(current_score))
+	
 
-func compute_score(offset_window: float) -> void:
-	current_score += 500 * (ambiance_progress_bar.max_value - ambiance_progress_bar.value/ambiance_progress_bar.max_value)
+
+func _on_mix_table_track_switched() -> void:
+	reset_ambiance_bar()
