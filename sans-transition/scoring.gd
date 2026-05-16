9bc0cc1f08e0a3a6825f5a92c67ef433cc42a935
Cédric Thiriet-Remondin <thiriet.remondin.cedric@gmail.com>
2026-05-16 10:29:57 +0200

    feat : track time management

--- sans-transition/scoring.gd
+++ sans-transition/scoring.gd
@@ -1,43 +1,62 @@
 extends Node
 
+@export var total_track_time_seconds: float
+
 @export var scoring_label : Label
-@export var ambiance_progress_bar: TextureProgressBar
+@export var total_track_time_pb: TextureProgressBar
+var _track_time: float
 
 @export var scoring_curve: Curve
-@export var max_range: float = 7
-var _current_time: float
+@export var max_range_scoring: float = 7
+
+
+signal track_finished()
+
+var excitment: float 
+
+var _scoring_time: float
 
 var current_score: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	current_score = 0.0
+	total_track_time_pb.max_value = total_track_time_seconds
+	_track_time = total_track_time_seconds
 	
-	reset_ambiance_bar()
+	reset_excitement()
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var t: float = 1-(_current_time/max_range)
-	print(t)
+	var t: float = 1-excitment
 	
 	current_score += scoring_curve.sample(t) * delta
 	
-	_current_time -= delta
-	_current_time = max(_current_time,0)
+	_scoring_time -= delta
+	_track_time -= delta
+	print(_track_time)
+
+	_scoring_time = max(_scoring_time,0)
 	
 	update_ui()
+	
+	if _track_time <= 0:
+		track_finished.emit()
 
-func reset_ambiance_bar() -> void:
-	_current_time = max_range
+func reset_excitement() -> void:
+	_scoring_time = max_range_scoring
 	
 func update_ui() -> void:
-	var t: float = _current_time/max_range
-	ambiance_progress_bar.value = t
+	excitment = _scoring_time/max_range_scoring
+	total_track_time_pb.value = _track_time
 	scoring_label.text = str(floori(current_score))
 	
 
 
 func _on_mix_table_track_switched() -> void:
-	reset_ambiance_bar()
+	reset_excitement()
+
+func get_excitement() -> float:
+	return excitment
