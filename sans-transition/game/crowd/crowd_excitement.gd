d513e57660019ce465d8938b2c997e808f8c2c8d
Nolkaloid <noe.le.cam.nlc@gmail.com>
2026-05-16 11:19:39 +0200

    feat: sync crowd excitement

--- sans-transition/game/crowd/crowd_excitement.gd
+++ sans-transition/game/crowd/crowd_excitement.gd
@@ -1,4 +1,5 @@
 @tool
+class_name Crowd
 extends Node
 
 @export var excitement: float:
@@ -13,11 +14,9 @@ extends Node
 
 @export var crowd_layers: Array[PeopleLayer]
 
-
 func _update_excitement() -> void:
 	for c in crowd_layers:
 		c.jump_scale = 0.1 + excitement
-
 
 func _on_tutorial_ended() -> void:
 	self.visible = true
--- sans-transition/scoring.gd
+++ sans-transition/scoring.gd
@@ -1,3 +1,4 @@
+class_name Scoring
 extends Node
 
 
